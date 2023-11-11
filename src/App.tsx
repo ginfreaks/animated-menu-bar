2e693a8ed595365fd0c5b8daf7e90d4041ea231a
rashyd <laanaya.rashid@gmail.com>
2023-11-11 01:29:28 +0100

    implement animation from framer motion to a hamburger menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Topbar } from "./components/topbar"
 
+ 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div className='min-h-screen flex justify-center p-5 items-center'>
+      <Topbar/>
+       <div className="space-y-5 text-center gap-5">
+        <h1 className="lg:text-4xl text-2xl font-bold">
+          Animated Menu Bar
+        </h1>
+        <p className="max-w-lg text-sm leading-6">
+          This is a simple example of how to build an animated menu bar or (hamburger menu) with 
+          React and Framer Motion.
         </p>
+        <button className="px-5 py-3 bg-neutral-300 rounded-md text-sm text-stone-800">
+          Get Started
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
